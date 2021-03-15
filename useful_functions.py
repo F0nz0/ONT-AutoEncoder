193c53c07453e93e34cb6bd35858b3f1b9fa398b
F0nz0 <41163796+F0nz0@users.noreply.github.com>
2021-03-15 12:55:59 +0100

    Add files via upload

---
+++ useful_functions.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+def get_mae(signal, model):
+    '''
+    Function to get the reconstruction MAE error from a raw signal after the transformation in time_steps
+    version and prediction of the reconstructed signal.
+    '''
+    X_scaled = StandardScaler().fit_transform(signal.dropna().values.reshape(-1,1))
+    time_steps = create_sequences(X_scaled)
+    time_step_pred = model.predict(time_steps)
+    mae_loss = np.mean(np.abs(time_step_pred - time_steps.reshape(time_steps.shape[0], time_steps.shape[1], 1)), axis=1)
+    return mae_loss
+
+
+def create_sequences(values, time_steps):
+    '''
+    Function to create a time_step version of the sequence.
+    '''
+    output=[]
+    for i in range(len(values)+1 - time_steps):
+        output.append(values[i:(i+time_steps)])
+    return np.stack(output).reshape(len(output), time_steps, 1)
+
+
+def recostruct_sequence(timestep_sequence):
+    '''
+    Function to reconstruct the sequence starting
+    from the time_step version.
+    '''
+    output = []
+    output += list(timestep_sequence[0].ravel())
+    for i in timestep_sequence[1:]:
+        output.append( i.ravel()[-1] )
+    #output += list(timestep_sequence[-1].ravel())
+    output = np.array(output)
+    return output
